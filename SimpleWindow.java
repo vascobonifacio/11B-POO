0af503192ea6804ad472685ef951a9d5a168caa9
WorkforceForTheFuture <37378843+carlos-f-carvalho@users.noreply.github.com>
2019-01-16 12:01:20 +0000

    Add files via upload

---
+++ SimpleWindow.java
@@ -0,0 +1,30 @@
+
+package poo11B;
+
+import java.awt.*;
+
+import javax.swing.*;
+// Create a simple GUI window
+
+public class SimpleWindow {
+   private static void createWindow() {
+
+      // Create and set up the window.
+      JFrame frame = new JFrame("Simple GUI");
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+      JLabel textLabel = new JLabel("I'm a label in the window", SwingConstants.CENTER);
+      textLabel.setPreferredSize(new Dimension(300, 100));
+      frame.getContentPane().add(textLabel, BorderLayout.CENTER);
+
+      // Display the window.
+      frame.setLocationRelativeTo(null);
+      frame.pack();
+      frame.setVisible(true);
+
+   }
+
+   public static void main(String[] args) {
+      createWindow();
+   }
+}
