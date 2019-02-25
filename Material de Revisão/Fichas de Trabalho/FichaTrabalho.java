53b95be8de8c4642c32333b2107b5954b3f5decb
Nuno <nuno.aja@gmail.com>
2019-02-25 18:55:23 +0000

    small update

--- Material de Revisão/Fichas de Trabalho/FichaTrabalho.java
+++ Material de Revisão/Fichas de Trabalho/FichaTrabalho.java
@@ -2,8 +2,8 @@ import java.util.ArrayList;
 
 public class FichaTrabalho {
 
-	// ExercÌcio 1
-	public double valorMedio(int array[]) { // Vasco Lopes (que disse ser JosÈ Rufino) 
+	// Exerc√≠cio 1
+	public double valorMedio(int array[]) { // Vasco Lopes / Jos√© Rufino
 		double soma = 0;
 		for(int e : array) {
 			soma = soma + e;
@@ -11,13 +11,13 @@ public class FichaTrabalho {
 		return soma / array.length;
 	}
 
-	// ExercÌcio 2
+	// Exerc√≠cio 2
 	public void imprimeComprida(String s1, String s2) { // Miguel Cruz
 		// System.out.println("Apagar isto e programar funcionalidade!");
 		System.out.println("Nome 1 => " + s1 + "  -- nome 2 => " + s2);
 	}
 
-	// ExercÌcio 3
+	// Exerc√≠cio 3
 	public void imprimeNomes(String[] nomes) { // Joaquim Queiroz
 		// a resolver pelo aluno !
 		for(String e : nomes) {
@@ -25,7 +25,7 @@ public class FichaTrabalho {
 		}
 	}
 
-	// ExercÌcio 5 -- a implementar pelo aluno
+	// Exerc√≠cio 5 -- a implementar pelo aluno
 
 
 	@SuppressWarnings("serial")
@@ -40,11 +40,11 @@ public class FichaTrabalho {
 
 		// Parte 1 ----------------------------------------------------------------------------------------
 
-		// ExercÌcio 1 - Teste
+		// Exerc√≠cio 1 - Teste
 		double media = ficha1.valorMedio(arrayTeste);
-		System.out.println("Valor mÈdio: " + media);
+		System.out.println("Valor m√©dio: " + media);
 
-		// ExercÌcio 2 - Teste
+		// Exerc√≠cio 2 - Teste
 		ficha1.imprimeComprida(nome1, nome2);
 
 		// Parte 2 ----------------------------------------------------------------------------------------
@@ -54,26 +54,26 @@ public class FichaTrabalho {
 				"Carolina Manso",
 				"Duarte Rodrigues",
 				"Joaquim Queiroz",
-				"JosÈ Rufino",
-				"LuÌs Matoso",
+				"Jos√© Rufino",
+				"Lu√≠s Matoso",
 				"Miguel Cruz",
 				"Miguel Freitas",
 				"Miguel Mendes",
 				"Nuno Pestana",
 				"Rodrigo Ferreira",
-				"Tiago Dur„o",
-				"Tom·s Pedro",
-				"Tom·s Regado",
+				"Tiago Dur√£o",
+				"Tom√°s Pedro",
+				"Tom√°s Regado",
 				"Vasco Lopes"
 		};
 		
 		int[] alturasAlunos = { 171, 169, 170, 168, 168, 172, 179, 190, 171, 175, 178, 140, 169, 165 };
 
-		// ExercÌcio 3 - Imprimir os nomes de todos os alunos usando um ciclo (while/for)
+		// Exerc√≠cio 3 - Imprimir os nomes de todos os alunos usando um ciclo (while/for)
 
-		// ExercÌcio 4 - Imprimir as mÈdias das alturas
+		// Exerc√≠cio 4 - Imprimir as m√©dias das alturas
 
-		// ExercÌcio 5 - Imprimir as notas dos alunos (junto aos seus nomes)
+		// Exerc√≠cio 5 - Imprimir as notas dos alunos (junto aos seus nomes)
 
 	}
 }
